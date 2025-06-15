544631ad5533ccd9eded07579d6c96a187e0731f
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-15 18:43:00 +0530

    Hello nworld

---
+++ 10-install-command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# id -u ----> If we will get 0 means root otherwise normal user
+
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]
+then
+    echo " Running with root user"
+else
+    echo " Running with normal user "
+
