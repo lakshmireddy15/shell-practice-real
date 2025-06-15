a9a2a941b578596c95b92873fe6fe0e88357f3e2
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-15 17:36:50 +0530

    Hello nworld

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1= Ram
-PERSON2= Sita   
+PERSON1=Ram
+PERSON2=Sita   
 echo "$PERSON1" : "Hi $PERSON2, How are you $PERSON2?"
 echo "$PERSON2" : "Hi $PERSON1, i am fine $PERSON1. What about you $PERSON1?"
 echo "$PERSON1" : "I am good $PERSON2"
