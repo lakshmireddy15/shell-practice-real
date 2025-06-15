86ffc1501433fd645d6fcbdf319fab3a252cc811
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-15 18:58:34 +0530

    Hello nworld

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -10,18 +10,25 @@ then
     echo " Running with root user"
 else
     echo " Running with normal user "
-    #exit 1
+    exit 1
 
 fi
 
 dnf list installed mysql
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    echo " Mysql is already installed"
-    #exit 0
-else
-
     echo "Mysql is not installed"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Mysql is sucessfully installed"
+    else
+        echo "Mysql is installation failed"
+    fi
+else
+    echo " Mysql is already installed"
+    exit 0
+
 fi
 
 
