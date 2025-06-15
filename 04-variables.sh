3207fc2cb56e20700438f9e5697d091fd1100cc8
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-15 17:38:15 +0530

    Hello nworld

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2   
+echo "$PERSON1" : "Hi $PERSON2, How are you $PERSON2?"
+echo "$PERSON2" : "Hi $PERSON1, i am fine $PERSON1. What about you $PERSON1?"
+echo "$PERSON1" : "I am good $PERSON2"
+
