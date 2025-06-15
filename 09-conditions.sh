b384e8bf2bbe4a47c54c896836cc6b0af7067fbf
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-15 18:26:34 +0530

    Hello nworld

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 NUM=30
-if [$NUM -eq 30];
+if [ $NUM -eq 30 ]
 then
     echo "Number is equal to 30"
 fi
 
-if [$NUM -ne 30]
+if [ $NUM -ne 30 ]
 then
     echo "Number is not equal to 30"
 fi
 
-if [$NUM -gt 30]
+if [ $NUM -gt 30 ]
 then
     echo "Number is greather than to 30"
 fi
 
-if [$NUM -ge 30]
+if [ $NUM -ge 30 ]
 then
     echo "Number is greather to 30"
 fi
 
-if [$NUM -lt 30]
+if [ $NUM -lt 30 ]
 then
     echo "Number is less than to 30"
 fi
 
-if [$NUM -le 30]
+if [ $NUM -le 30 ]
 then
     echo "Number is less than or equal to 30"
 fi
