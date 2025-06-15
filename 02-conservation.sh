6773e7bc37f9a8c6305c7724b80c33da38f95d1e
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-15 17:33:24 +0530

    Hello nworld

--- 02-conservation.sh
+++ 02-conservation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-"Ram" : "Hi sita, How are you sita?"
-"sita" : "Hi Ram, i am fine Ram. What about you Ram?"
-"Ram" : "I am good Sita"
+echo "Ram" : "Hi sita, How are you sita?"
+echo "sita" : "Hi Ram, i am fine Ram. What about you Ram?"
+echo "Ram" : "I am good Sita"
 
