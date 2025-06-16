4c93cbd6e5269a2c891fbe86b5cb8d6c9695b1e5
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-16 12:30:49 +0530

    shell

--- 12-colors.sh
+++ 12-colors.sh
@@ -5,3 +5,12 @@
 echo -e "\e[31m Hello world \e[0m "
 
 echo -e " Hello World"
+
+echo -e "\e[32m Hello world \e[0m "
+
+echo -e " Hello World"
+
+
+echo -e "\e[33m Hello world \e[0m "
+
+echo -e " Hello World"
